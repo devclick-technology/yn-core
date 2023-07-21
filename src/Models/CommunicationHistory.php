9d77752496558cc6939a7f532c92e7ecf3d778d0
Sagar Naliyapara <naliyaparaspn@gmail.com>
2023-07-21 20:15:02 +0530

    Add missing relationships

--- src/Models/CommunicationHistory.php
+++ src/Models/CommunicationHistory.php
@@ -8,4 +8,42 @@ use YouNegotiate\Models\Interfaces\ICommunicationHistory;
 class CommunicationHistory extends Model implements ICommunicationHistory
 {
     protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function consumer()
+    {
+        return $this->belongsTo(Consumer::class)->withDefault(['full_name' => '']);
+    }
+
+    public function group()
+    {
+        return $this->belongsTo(Group::class)->withDefault(['name' => '']);
+    }
+
+    public function template()
+    {
+        return $this->belongsTo(Template::class)->withDefault(['name' => '', 'type' => '']);
+    }
+
+    public function templateType()
+    {
+        $template_type = null;
+        if ($this->template_type == null) {
+            if ($this->template_id) {
+                $template_type = $this->template->type;
+            } elseif ($this->email != null) {
+                $template_type = 'email';
+            } else {
+                $template_type = 'sms';
+            }
+        } else {
+            $template_type = $this->template_type;
+        }
+
+        return $template_type;
+    }
 }
