f20a940c1525bd35b5f9d090fc2fdbaa7b052eda
Karan Bijvani <karanbijvani@gmail.com>
2023-07-26 11:16:28 +0530

    add softdelete in PaymentProfile

--- src/Models/PaymentProfile.php
+++ src/Models/PaymentProfile.php
@@ -5,11 +5,13 @@ namespace YouNegotiate\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use YouNegotiate\Models\Interfaces\IPaymentProfile;
 
 class PaymentProfile extends Model implements IPaymentProfile
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'consumer_id',
