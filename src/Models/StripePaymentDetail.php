f010a7c8511c45c13d34c8e48a45d29473b27f74
Sagar Naliyapara <naliyaparaspn@gmail.com>
2023-07-31 17:03:47 +0530

    add soft delete in stripe payment details

--- src/Models/StripePaymentDetail.php
+++ src/Models/StripePaymentDetail.php
@@ -3,9 +3,12 @@
 namespace YouNegotiate\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use YouNegotiate\Models\Interfaces\IStripePaymentDetail;
 
 class StripePaymentDetail extends Model implements IStripePaymentDetail
 {
+    Use SoftDeletes;
+
     protected $table = 'stripe_payment_detail';
 }
