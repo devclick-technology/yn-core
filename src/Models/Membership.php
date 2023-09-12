17d9ef41e9a941b1aa88d2d019aa2228a197139c
Sagar Naliyapara <naliyaparaspn@gmail.com>
2023-09-12 14:10:29 +0530

    add softdeletes in membership model

--- src/Models/Membership.php
+++ src/Models/Membership.php
@@ -3,9 +3,12 @@
 namespace YouNegotiate\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use YouNegotiate\Models\Interfaces\IMembership;
 
 class Membership extends Model implements IMembership
 {
+    use SoftDeletes;
+    
     protected $guarded = ['id', 'created_at', 'updated_at', 'deleted_at'];
 }
