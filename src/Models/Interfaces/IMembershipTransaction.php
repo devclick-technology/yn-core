a4e354fd53987a82de74f04d136b0051804485b1
Sagar Naliyapara <naliyaparaspn@gmail.com>
2023-09-21 20:16:23 +0530

    Add membership transaction model with interface

---
+++ src/Models/Interfaces/IMembershipTransaction.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace YouNegotiate\Models\Interfaces;
+
+use Illuminate\Database\Eloquent\Casts\Json;
+use Illuminate\Support\Carbon;
+use YouNegotiate\Models\Company;
+use YouNegotiate\Models\Membership;
+
+/**
+ * @property int $id
+ * @property string $status
+ * @property float $price
+ * @property Json|null $response
+ * @property string|null $tilled_transaction_id
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Company|null $company
+ * @property-read Membership|null $membership
+ */
+
+interface IMembershipTransaction
+{
+}
