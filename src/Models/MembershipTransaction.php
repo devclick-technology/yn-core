142dbff1b369e1c84184b78ff9a0ddb507c822dd
Sagar Naliyapara <naliyaparaspn@gmail.com>
2023-09-21 20:16:23 +0530

    Add membership transaction model with interface

---
+++ src/Models/MembershipTransaction.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace YouNegotiate\Models;
+
+use YouNegotiate\Models\Interfaces\IMembershipTransaction;
+
+class MembershipTransaction extends BaseModel implements IMembershipTransaction
+{
+}
