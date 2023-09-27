0eb69ab46ebb71a91b4fcd8e556ea2162c515a2c
Sagar Naliyapara <naliyaparaspn@gmail.com>
2023-09-27 16:12:56 +0530

    membership relation add in MembershipTransection

--- src/Models/MembershipTransaction.php
+++ src/Models/MembershipTransaction.php
@@ -2,8 +2,13 @@
 
 namespace YouNegotiate\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use YouNegotiate\Models\Interfaces\IMembershipTransaction;
 
 class MembershipTransaction extends BaseModel implements IMembershipTransaction
 {
+    public function membership(): BelongsTo
+    {
+        return $this->belongsTo(Membership::class, 'membership_id');
+    }
 }
