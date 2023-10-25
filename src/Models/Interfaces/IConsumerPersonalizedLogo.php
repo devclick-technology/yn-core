33967cd43619a848a31b220b3ffb4fab077286c3
Sagar Naliyapara <naliyaparaspn@gmail.com>
2023-10-25 11:22:15 +0530

    - add new modal and its relation ship

---
+++ src/Models/Interfaces/IConsumerPersonalizedLogo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace YouNegotiate\Models\Interfaces;
+
+use Illuminate\Support\Carbon;
+/**
+ * @property int $id
+ * @property int $consumer_id
+ * @property string|null $primary_color
+ * @property string|null $secondary_color
+ * @property string|null $background_color
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ */
+interface IConsumerPersonalizedLogo
+{
+}
