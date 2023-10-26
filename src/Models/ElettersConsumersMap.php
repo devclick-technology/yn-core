cf72dd5ce6a25cdde065c71e32eaa3af7bf5296b
Sagar Naliyapara <naliyaparaspn@gmail.com>
2023-10-26 12:44:35 +0530

    e letters relationship correction with template to Eletters

--- src/Models/ElettersConsumersMap.php
+++ src/Models/ElettersConsumersMap.php
@@ -2,17 +2,19 @@
 
 namespace YouNegotiate\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class ElettersConsumersMap extends BaseModel
 {
     protected $table = 'eletters_consumers_map';
 
-    public function consumer()
+    public function consumer(): BelongsTo
     {
         return $this->belongsTo(Consumer::class);
     }
 
-    public function eletter()
+    public function eletter(): BelongsTo
     {
-        return $this->belongsTo(Template::class, 'eletters_id', 'id');
+        return $this->belongsTo(Eletter::class, 'eletters_id');
     }
 }
