467952fd5fd28fe37e28988dc6b02b4e1a75e4b1
Sagar Naliyapara <naliyaparaspn@gmail.com>
2023-11-10 11:02:01 +0530

    remove property background_color from ConsumerPersonalizedLogo Interface

--- src/Models/Interfaces/IConsumerPersonalizedLogo.php
+++ src/Models/Interfaces/IConsumerPersonalizedLogo.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Carbon;
  * @property int $consumer_id
  * @property string|null $primary_color
  * @property string|null $secondary_color
- * @property string|null $background_color
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
  */
