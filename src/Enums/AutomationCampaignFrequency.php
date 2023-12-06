a18db30e93642ea8ff4e5347948262071fee81ec
Divyesh Vadoliya <d.a.vadoliya@gmail.com>
2023-12-06 13:17:54 +0530

    create function for select box key and value

--- src/Enums/AutomationCampaignFrequency.php
+++ src/Enums/AutomationCampaignFrequency.php
@@ -7,10 +7,19 @@ use YouNegotiate\Enums\Traits\Values;
 
 enum AutomationCampaignFrequency: string
 {
+    use Names;
     use Values;
     case ONCE = 'once';
     case HOURLY = 'hourly';
     case DAILY = 'daily';
     case WEEKLY = 'weekly';
     case MONTHLY = 'monthly';
+
+    public static function displaySelectionBox(): array
+    {
+        return collect(self::cases())->mapWithKeys(fn ($case): array => [
+            $case->displayName() => $case->value,
+        ])->toArray();
+
+    }
 }
