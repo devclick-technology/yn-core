847f6c7b56f3c8fb4cf4af2339f69de49ed24730
Sagar Naliyapara <naliyaparaspn@gmail.com>
2023-12-07 11:00:14 +0530

    membership changes according new migrations and remove interface

--- src/Models/CompanyMembership.php
+++ src/Models/CompanyMembership.php
@@ -4,13 +4,26 @@ namespace YouNegotiate\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use YouNegotiate\Models\Interfaces\ICompanyMembership;
 
-class CompanyMembership extends Model implements ICompanyMembership
+class CompanyMembership extends Model
 {
     public $timestamps = false;
 
-    protected $guarded = [];
+    protected $fillable = [
+        'company_id',
+        'membership_id',
+        'status',
+        'current_plan_start',
+        'current_plan_end',
+        'auto_renew',
+        'next_membership_plan_id',
+        'cancelled_at',
+    ];
+
+    protected $casts = [
+        'current_plan_start' => 'datetime',
+        'current_plan_end' => 'datetime',
+    ];
 
     public function company(): BelongsTo
     {
