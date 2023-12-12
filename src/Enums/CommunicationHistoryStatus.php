95446d38fae6c08d284ad5b66a28eca63a0cbe8d
Utsav Somaiya <utsavsomaiya4464@gmail.com>
2023-12-12 22:42:26 +0530

    add: display selection box method into the communication history status enum

--- src/Enums/CommunicationHistoryStatus.php
+++ src/Enums/CommunicationHistoryStatus.php
@@ -4,9 +4,20 @@ declare(strict_types=1);
 
 namespace YouNegotiate\Enums;
 
+use YouNegotiate\Enums\Traits\Names;
+
 enum CommunicationHistoryStatus: int
 {
+    use Names;
+
     case SUCCESS = 1;
     case FAILED = 2;
     case IN_PROGRESS = 3;
+
+    public static function displaySelectionBox(): array
+    {
+        return collect(self::cases())->mapWithKeys(fn ($case): array => [
+            $case->value => $case->displayName(),
+        ])->toArray();
+    }
 }
