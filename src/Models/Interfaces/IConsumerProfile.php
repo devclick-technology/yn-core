7c71af3b72d0e80d47afd10fff13be0fe89d3e67
Utsav Somaiya <utsavsomaiya4464@gmail.com>
2023-12-13 10:58:49 +0530

    improve: iconsumerprofile interface

--- src/Models/Interfaces/IConsumerProfile.php
+++ src/Models/Interfaces/IConsumerProfile.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Carbon;
  * @property string|null $mobile
  * @property string|null $landline
  * @property string|null $email
+ * @property int $text_permission
+ * @property int $email_permission
+ * @property int $mobile_call_permission
+ * @property int $landline_call_permission
+ * @property int $usps_permission
+ * @property string|null $email
  * @property string|null $image
  * @property string|null $username
  * @property Carbon|null $created_at
