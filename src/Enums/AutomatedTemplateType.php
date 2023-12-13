3f8f246c4bdff9f64d449e5240ac2b1afc866235
Utsav Somaiya <utsavsomaiya4464@gmail.com>
2023-12-13 22:58:59 +0530

    add selection box

--- src/Enums/AutomatedTemplateType.php
+++ src/Enums/AutomatedTemplateType.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace YouNegotiate\Enums;
 
-use YouNegotiate\Enums\Traits\Names;
+use YouNegotiate\Enums\Traits\SelectionBox;
 use YouNegotiate\Enums\Traits\Values;
 
 enum AutomatedTemplateType: string
 {
+    use SelectionBox;
     use Values;
-    use Names;
 
     case EMAIL = 'email';
     case SMS = 'sms';
