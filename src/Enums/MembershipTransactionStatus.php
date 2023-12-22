6c0a8a964d7bb7ce07c90c8d3013a3ab5293f56c
Utsav Somaiya <utsavsomaiya4464@gmail.com>
2023-12-22 11:00:19 +0530

    use name trait in membership transaction status

--- src/Enums/MembershipTransactionStatus.php
+++ src/Enums/MembershipTransactionStatus.php
@@ -2,8 +2,12 @@
 
 namespace YouNegotiate\Enums;
 
+use YouNegotiate\Enums\Traits\Names;
+
 enum MembershipTransactionStatus: string
 {
+    use Names;
+
     case SUCCESS = 'success';
     case FAILED = 'failed';
 }
