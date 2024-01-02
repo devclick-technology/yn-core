7597f275cdbb17c56393cb348632ed9d713b6c9b
Utsav Somaiya <utsavsomaiya4464@gmail.com>
2024-01-02 16:01:32 +0530

    fix: pint and add order by for membership settings

--- src/Enums/BankAccountType.php
+++ src/Enums/BankAccountType.php
@@ -2,7 +2,6 @@
 
 namespace YouNegotiate\Enums;
 
-
 use YouNegotiate\Enums\Traits\Names;
 use YouNegotiate\Enums\Traits\SelectionBox;
 use YouNegotiate\Enums\Traits\Values;
@@ -10,8 +9,8 @@ use YouNegotiate\Enums\Traits\Values;
 enum BankAccountType: string
 {
     use Names;
-    use Values;
     use SelectionBox;
+    use Values;
 
     case CHECKING = 'checking';
     case SAVINGS = 'savings';
